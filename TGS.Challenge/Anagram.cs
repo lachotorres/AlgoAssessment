fd6d81d605c0b3177c2947202a0c74f6972b7469
Horacio Torres <horaciotg@scubacode.com>
2021-01-13 21:39:05 -0600

    Challenge 1:  Check if 2 words are anagrams of one another.

--- TGS.Challenge/Anagram.cs
+++ TGS.Challenge/Anagram.cs
@@ -1,30 +1,54 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace TGS.Challenge
 {
-  /*
-        Devise a function that checks if 1 word is an anagram of another, if the words are anagrams of
-        one another return true, else return false
+    /*
+          Devise a function that checks if 1 word is an anagram of another, if the words are anagrams of
+          one another return true, else return false
 
-        "Anagram": An anagram is a type of word play, the result of rearranging the letters of a word or
-        phrase to produce a new word or phrase, using all the original letters exactly once; for example
-        orchestra can be rearranged into carthorse.
+          "Anagram": An anagram is a type of word play, the result of rearranging the letters of a word or
+          phrase to produce a new word or phrase, using all the original letters exactly once; for example
+          orchestra can be rearranged into carthorse.
 
-        areAnagrams("horse", "shore") should return true
-        areAnagrams("horse", "short") should return false
+          areAnagrams("horse", "shore") should return true
+          areAnagrams("horse", "short") should return false
 
-        NOTE: Punctuation, including spaces should be ignored, e.g.
+          NOTE: Punctuation, including spaces should be ignored, e.g.
 
-        horse!! shore = true
-        horse  !! shore = true
-          horse? heroes = true
+          horse!! shore = true
+          horse  !! shore = true
+            horse? heroes = true
 
-        There are accompanying unit tests for this exercise, ensure all tests pass & make
-        sure the unit tests are correct too.
-     */
+          There are accompanying unit tests for this exercise, ensure all tests pass & make
+          sure the unit tests are correct too.
+    */
     public class Anagram
     {
-      public bool AreAnagrams(string word1, string word2)
-      {
-        return false;
-      }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="word1"></param>
+        /// <param name="word2"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// NOTE horse and heroes cannot be an Anagrams cause definition of angaram says we have to use all the original letters only once
+        /// so heroes contain 2 "e" chars and cannot be an anagram of horse
+        /// Also "Funeral" and "Reel fun" cannot be anagrams so assert to true on test case "Funeral_IsAnagram_Reel_fun" is wrong.
+        /// </remarks>
+        public bool AreAnagrams(string word1, string word2)
+        {
+            if (string.IsNullOrEmpty(word1) || string.IsNullOrEmpty(word2)) throw new ArgumentException();
+
+            var w1 = Regex.Replace(word1, "[^a-zA-Z]", "").ToLower();
+            var w2 = Regex.Replace(word2, "[^a-zA-Z]", "").ToLower();
+
+            w1 = Helper.Sort(w1);
+            w2 = Helper.Sort(w2);
+
+            return w1.Equals(w2);
+        }
+
+        
     }
 }
---
+++ TGS.Challenge/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TGS.Challenge
+{
+    public class Helper
+    {
+        /// <summary>
+        /// Sorts string chars alphabetically
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// This can be done easier with LINQ but I think there is no point to use LINQ on this assesment.
+        /// </remarks>
+        public static string Sort(string str)
+        {
+            char temp;
+            char[] charstr = str.ToCharArray();
+            for (int i = 1; i < charstr.Length; i++)
+            {
+                for (int j = 0; j < charstr.Length - 1; j++)
+                {
+                    if (charstr[j] > charstr[j + 1])
+                    {
+                        temp = charstr[j];
+                        charstr[j] = charstr[j + 1];
+                        charstr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return new string(charstr);
+        }
+    }
+}
